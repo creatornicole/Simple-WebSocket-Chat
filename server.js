25d6c5842e3d781f4310b8b7b14808d2eec76749
Nicole Gottschall <ngottschall@web.de>
2023-05-11 15:25:34 +0200

    adds WebSocket Connection

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var server = http.createServer(app);
 var wss = new WebSocket.Server({ server });
 var chats = new Map(); // chatId => [socket connection 1, socket connection 2, ...]
 
+server.listen(3000); //Port
+
 //fuer eventuelle Einbindung style.css in Ordner public
 //app.use(express.static(path.join(__dirname, '/public/')));
 
@@ -23,9 +25,59 @@ var chats = new Map(); //diese Variable wird nur im Speicher gehalten
 app.get('/', function(req, res) {
     const characters = "abcdefghijklmnopqrstuvwxyz1234567890";
     var chatIdString;
-    for(let i = 0; i <= 7; i++) {
+    for(let i = 0; i <= 7; i++) { //generate random Chat-Id
         chatIdString += characters.charAt(Math.floor(Math.random() * characters.length));
     }
 
-    
+    //zum Key chatIdString wird ein leeres Array angelegt, in dem die Websocketverbindungen
+    //gespeichert werden sollen
+    chats.set(chatIdString, []);
+
+    res.redirect('/chat/' + chatIdString);
 });
+
+//Anzeige chat.html, wenn Nutzer auf eine Chat-Seite gelangt
+app.get('/chat/:chatId', function(req, res) {
+    var chatId = req.params.chatId; //die Chat-Id wird aus der URL gewonnen
+    if(!chats.has(chatId)) { //chatId in Map noch nicht vorhanden
+        chats.set(chatId, []);
+    }
+    res.sendFile(path.join(__dirname, '../WebSockets', 'chat.html'))
+});
+
+//Websocketverbindung
+wss.on('connection', function connection(ws, request) {
+    ws.on('message', function incoming(message) {
+        var chatId = JSON.parse(message).chatId;
+        //pruefen, ob Verbindung bereits ChatId zugeordnet ist
+        if(chats.has(chatId)) {
+            if(!chats.get(chatId).includes(ws)) {
+                //wenn Connection noch nicht aufgenommen wurde, hinzufügen
+                chats.get(chatId).push(ws);
+            }
+        }
+
+        messageJSON = JSON.parse(message);
+
+        //Reaktion bei init Nachricht
+        if(messageJSON.type == 'init') {
+            var reply = {};
+            reply["message"] = "Reply on Init-Message from Chat: " + chatId;
+            ws.send(JSON.stringify(reply));
+        }
+
+        //Reaktion bei normaler Nachricht
+        if(messageJSON.type == 'message') {
+            connections = chats.get(chatId);
+            connections.forEach(sendMsg);
+        }
+
+        function sendMsg(con) {
+            var reply = {};
+            reply["message"] = messageJSON.message;
+            reply["name"] = messageJSON.name;
+            reply["type"] = "message";
+            con.send(JSON.stringify(reply));
+        }
+    })
+})
